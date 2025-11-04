b6a91d3994672df850a0e66207c21050dba0bc58
Cedric <crubbertire@rogers.com>
2025-11-04 14:59:26 -0500

    Created form and fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
 // src/App.jsx
+import { useState } from 'react';
+const apiKey = import.meta.env.VITE_API_KEY;
 
 const App = () => {
+  const [city, setCity] = useState("");
+
+  const handleChange = (event) => {
+    setCity(event.target.value);
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    let response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`)
+    let data = await response.json();
+    console.log(data)
+  }
 
   return (
-    <h1>Hello world!</h1>
+    <>
+      <form onSubmit={handleSubmit}>
+        Please enter your city name for the weather: {city} 
+        <br></br>
+        <input type="text" name="city" onChange={handleChange} />
+        <button type="submit">Get my forecast!</button>
+      </form>
+    </>
   );
 }
 
